7ea735c2288e99f12e57c5436d40af0c68adb945
asifkottakal1 <asifkottakal1@gmail.com>
2020-06-16 13:56:36 +0530

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+
+@section('title')
+Dashboard
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-md-12" >
+      <a href="/generate-shorten-link"><div class="card" style="background-color: grey;text-align:center">
+        <div class="card-header">
+          <h4 class="card-title">Urls</h4>
+        </div>
+        <div class="card-body">
+          
+        </div>
+      </div>
+      </a>
+    </div>
+   
+  
+    
+  </div>
+@endsection
+
+@section('scripts')
+@endsection
---
+++ resources/views/shortenLink.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Url shortener </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" />
+</head>
+<body>
+   
+<div class="container">
+    <h1>Url shortener </h1>
+   
+    <div class="card">
+      <div class="card-header">
+        <form method="POST" action="{{ route('generate.shorten.link.post') }}">
+            @csrf
+            <div class="input-group mb-3">
+              <input type="text" name="link" class="form-control" placeholder="Enter URL" aria-label="Recipient's username" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-success" type="submit">Generate Shorten Link</button>
+              </div>
+            </div>
+        </form>
+      </div>
+      <div class="card-body">
+   
+            @if (Session::has('success'))
+                <div class="alert alert-success">
+                    <p>{{ Session::get('success') }}</p>
+                </div>
+            @endif
+   
+            <table class="table table-bordered table-sm">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Duplicate url</th>
+                        <th>Url</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($shortLinks as $row)
+                        <tr>
+                            <td>{{ $row->id }}</td>
+                            <td id="myInput"><a href="{{ route('shorten.link', $row->code) }}" target="_blank">{{ route('shorten.link', $row->code) }}</a></td>
+                            <td>{{ $row->link }}</td>
+                            <td>{{ $row->updated_at }}</td>
+                            <td><button onclick="myFunction()" class="btn btn-success">Copy</button></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+      </div>
+    </div>
+   
+</div>
+    
+</body>
+{{-- <script>
+    function myFunction() {
+      var copyText = document.getElementById("myInput");
+      copyText.select();
+      copyText.setSelectionRange(0, 99999)
+      document.execCommand("copy");
+      alert("Copied the text: " + copyText.value);
+    }
+    </script> --}}
+</html>
